e18db5f3c9d17eb75c3181117d7a5cf1448f6701
Jessica <166739896+JessicaTodor@users.noreply.github.com>
2024-04-12 02:36:39 +0300

    Add files via upload

---
+++ script-introduction.js
@@ -0,0 +1,96 @@
+function javascript_display(){
+    document.getElementById("javascript-txt").style.display="block";
+    document.getElementById("cs-txt").style.display="none";
+    document.getElementById("python-txt").style.display="none";
+    document.getElementById("java-txt").style.display="none";
+    document.getElementById("php-txt").style.display="none";
+    document.getElementById("sql-txt").style.display="none";
+
+    document.getElementById("javascript-lb").style.background="#794CEC";
+    document.getElementById("cs-lb").style.background="none";
+    document.getElementById("python-lb").style.background="none";
+    document.getElementById("java-lb").style.background="none";
+    document.getElementById("php-lb").style.background="none";
+    document.getElementById("sql-lb").style.background="none";
+
+}
+
+function cs_display(){
+    document.getElementById("javascript-txt").style.display="none";
+    document.getElementById("cs-txt").style.display="block";
+    document.getElementById("python-txt").style.display="none";
+    document.getElementById("java-txt").style.display="none";
+    document.getElementById("php-txt").style.display="none";
+    document.getElementById("sql-txt").style.display="none";
+
+    document.getElementById("javascript-lb").style.background="none";
+    document.getElementById("cs-lb").style.background="#794CEC";
+    document.getElementById("python-lb").style.background="none";
+    document.getElementById("java-lb").style.background="none";
+    document.getElementById("php-lb").style.background="none";
+    document.getElementById("sql-lb").style.background="none";
+}
+
+function python_display(){
+    document.getElementById("javascript-txt").style.display="none";
+    document.getElementById("cs-txt").style.display="none";
+    document.getElementById("python-txt").style.display="block";
+    document.getElementById("java-txt").style.display="none";
+    document.getElementById("php-txt").style.display="none";
+    document.getElementById("sql-txt").style.display="none";
+
+    document.getElementById("javascript-lb").style.background="none";
+    document.getElementById("cs-lb").style.background="none";
+    document.getElementById("python-lb").style.background="#794CEC";
+    document.getElementById("java-lb").style.background="none";
+    document.getElementById("php-lb").style.background="none";
+    document.getElementById("sql-lb").style.background="none";
+}
+
+function java_display(){
+    document.getElementById("javascript-txt").style.display="none";
+    document.getElementById("cs-txt").style.display="none";
+    document.getElementById("python-txt").style.display="none";
+    document.getElementById("java-txt").style.display="block";
+    document.getElementById("php-txt").style.display="none";
+    document.getElementById("sql-txt").style.display="none";
+
+    document.getElementById("javascript-lb").style.background="none";
+    document.getElementById("cs-lb").style.background="none";
+    document.getElementById("python-lb").style.background="none";
+    document.getElementById("java-lb").style.background="#794CEC";
+    document.getElementById("php-lb").style.background="none";
+    document.getElementById("sql-lb").style.background="none";
+}
+
+function php_display(){
+    document.getElementById("javascript-txt").style.display="none";
+    document.getElementById("cs-txt").style.display="none";
+    document.getElementById("python-txt").style.display="none";
+    document.getElementById("java-txt").style.display="none";
+    document.getElementById("php-txt").style.display="block";
+    document.getElementById("sql-txt").style.display="none";
+
+    document.getElementById("javascript-lb").style.background="none";
+    document.getElementById("cs-lb").style.background="none";
+    document.getElementById("python-lb").style.background="none";
+    document.getElementById("java-lb").style.background="none";
+    document.getElementById("php-lb").style.background="#794CEC";
+    document.getElementById("sql-lb").style.background="none";
+}
+
+function sql_display(){
+    document.getElementById("javascript-txt").style.display="none";
+    document.getElementById("cs-txt").style.display="none";
+    document.getElementById("python-txt").style.display="none";
+    document.getElementById("java-txt").style.display="none";
+    document.getElementById("php-txt").style.display="none";
+    document.getElementById("sql-txt").style.display="block";
+
+    document.getElementById("javascript-lb").style.background="none";
+    document.getElementById("cs-lb").style.background="none";
+    document.getElementById("python-lb").style.background="none";
+    document.getElementById("java-lb").style.background="none";
+    document.getElementById("php-lb").style.background="none";
+    document.getElementById("sql-lb").style.background="#794CEC";
+}
